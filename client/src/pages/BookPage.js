e289c2eb3288fb45b562ed64555e848cfcf3282e
Yasmin Dal Medico <yasmin.dm@gmail.com>
2023-05-09 11:03:35 +0200

    if not loggein cannot borrow

--- client/src/pages/BookPage.js
+++ client/src/pages/BookPage.js
@@ -1,8 +1,12 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { getOneBookServ } from "../Service/Books";
 import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { toast } from "react-toastify";
 
 function BookPage() {
+  const loggedin = useSelector((state)=>state.user.Loggedin)
+  const navigate = useNavigate()
     const [book, setBook] = useState([])
   const {isbn} = useParams()
   console.log(isbn)
@@ -14,17 +18,36 @@ function BookPage() {
     fetchBook()
     },[])
 
+  const handleBorrow=()=>{
+    if(!loggedin){
+      navigate('/login')
+      toast('Login first to borrow a book')
+    }else{
+      //how to do it?
+      toast('This book will be in your e-reader in some minutes')
+    }
+  }
+
+  const handleQueue=()=>{
+    if(!loggedin){
+      navigate('/login')
+      toast('Login first to borrow a book')
+    }else{
+      //how to do it?
+      toast('We will let you know when this book is available')
+    }
+  }
+
     return (
         <div className='main'>
             <section className="book">
                 <h2>{book.title}</h2>
                 <h3>{book.author}</h3>
                 <p>{book.description}</p>
-                {book.available > 0?<button>Borrow</button> : <button>Queue</button>}
+                {book.available > 0?<button onClick={handleBorrow}>Borrow</button> : <button onClick={handleQueue}>Queue</button>}
             </section>
       </div>
     );
   }
-  //navigate to login if not loggedin, change button to return if logged in
   
   export default BookPage;
