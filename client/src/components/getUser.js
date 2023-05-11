e42cf0f313a4d3f15b65f9542f0f9f988bff9629
Yasmin Dal Medico <yasmin.dm@gmail.com>
2023-05-11 17:51:16 +0200

    starting delete user by admin

--- client/src/components/getUser.js
+++ client/src/components/getUser.js
@@ -1,21 +1,26 @@
 import { FaTrash } from "react-icons/fa";
 const GetUser = (user)=>{
+    const id= user._id
     const handleBan=()=>{
      //create backend to ban
       }
       const handleUnban=()=>{
        // create backend to unban
       }
-    const deleteUser=()=>{
-        //delete user backend
-    }
+    const deleteUser=async (id)=>{
+        console.log(id)
+            // await deleteUserServ(user._id)
+            // navigate('/')
+            // toast('Profile deleted')
+        }
+
     return (
         <tr key={user._id}>
             <td>{user.name}</td>
             <td>{user.username}</td>
             <td>{user.email}</td>
             <td>{user.is_banned? <button onClick={handleUnban}>Unban</button>: <button onClick={handleBan}>Ban</button>}</td>
-            <td><a className="icon" href='/get-users' onClick={deleteUser}><FaTrash/></a></td>
+            <td><a className="icon" href='/get-users' onClick={()=>deleteUser(user._id)}><FaTrash/></a></td>
         </tr>
     )
 }
--- client/src/pages/GetUsers.js
+++ client/src/pages/GetUsers.js
@@ -12,7 +12,6 @@ const GetUsers = ()=> {
   useEffect(()=>{
   fetchAllUsers()
   },[])
-  console.log(users)
 
     return (
       <div className="main" >
