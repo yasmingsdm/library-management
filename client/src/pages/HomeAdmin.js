a0014b26536ca5a1cb4ec2fc017274757b9d9a96
Yasmin Dal Medico <yasmin.dm@gmail.com>
2023-05-11 16:27:46 +0200

    clean code

--- client/src/pages/HomeAdmin.js
+++ client/src/pages/HomeAdmin.js
@@ -1,10 +1,7 @@
 import Profile from "./Profile"
 import Admin from "./Admin"
-import { useLocation } from "react-router-dom"
+
 const HomeAdmin = ()=>{
-    const {state} = useLocation()
-    console.log(state)
-  
       return (
       <div className="main row">
             <Admin/>
