8175f1f0cbb082f2cdb9a54ed0174c144e934fea
Yasmin Dal Medico <yasmin.dm@gmail.com>
2023-06-05 10:42:54 +0200

    change base url for backend deployed

--- client/src/Service/Books.js
+++ client/src/Service/Books.js
@@ -1,31 +1,33 @@
 import axios from 'axios'
 
+const baseUrl = 'https://backend-vuk4.onrender.com/'
+
 export const createBookServ = async(book)=>{
-    const response = await axios.post('http://localhost:8001/book/', book)
+    const response = await axios.post(`${baseUrl}/book/`, book)
     return response
 }
 
 export const getAllBooksServ = async()=>{
-    const response = await axios.get('http://localhost:8001/book/search')
+    const response = await axios.get(`${baseUrl}/book/search`)
     return response.data
 }
 
 export const getAllBooksByPageServ = async(page)=>{
-    const response = await axios.get(`http://localhost:8001/book?page=${page}`)
+    const response = await axios.get(`${baseUrl}/book?page=${page}`)
     return response.data
 }
 
 export const getOneBookServ = async(isbn)=>{
-    const response = await axios.get(`http://localhost:8001/book/${isbn}`)
+    const response = await axios.get(`${baseUrl}/book/${isbn}`)
     return response
 }
 
 export const deleteBookServ = async(isbn)=>{
-    const response = await axios.delete(`http://localhost:8001/book/${isbn}`)
+    const response = await axios.delete(`${baseUrl}/book/${isbn}`)
     return response
 }
 
 export const updateBookServ = async(isbn,book)=>{
-    const response = await axios.put(`http://localhost:8001/book/${isbn}`, book)
+    const response = await axios.put(`${baseUrl}/book/${isbn}`, book)
     return response
 }
--- client/src/Service/Users.js
+++ client/src/Service/Users.js
@@ -1,74 +1,76 @@
 import axios from 'axios'
 
+const baseUrl = 'https://backend-vuk4.onrender.com'
+
 export const signupUserServ = async(user)=>{
-    const response = await axios.post('http://localhost:8001/user/signup', user)
+    const response = await axios.post(`${baseUrl}/user/signup`, user)
     return response
 }
 
 export const activateUserServ = async(token)=>{
     console.log({token})
-    const response = await axios.post('http://localhost:8001/user/verify-email', {token})
+    const response = await axios.post(`${baseUrl}/user/verify-email`, {token})
     console.log('aqui')
     return response
 }
 
 export const loginUserServ = async(user)=>{
-    const response = await axios.post('http://localhost:8001/user/login', user)
+    const response = await axios.post(`${baseUrl}/user/login`, user)
     return response
 }
 
 export const logoutUserServ = async()=>{
-    const response = await axios.get('http://localhost:8001/user/logout')
+    const response = await axios.get(`${baseUrl}/user/logout`)
     return response
 }
 
 export const getProfileServ = async(id)=>{
-    const response = await axios.get(`http://localhost:8001/user/${id}`)
+    const response = await axios.get(`${baseUrl}/user/${id}`)
     return response
 }
 
 export const getAllUsersServ = async()=>{
-    const response = await axios.get('http://localhost:8001/admin/dashboard/all-users')
+    const response = await axios.get(`${baseUrl}/admin/dashboard/all-users`)
     return response
 }
 
 export const updateUserServ = async(id,user)=>{
-    const response = await axios.put(`http://localhost:8001/user/${id}`, user)
+    const response = await axios.put(`${baseUrl}/user/${id}`, user)
     return response 
 }
 
 export const deleteUserServ = async(id)=>{
-    const response = await axios.delete(`http://localhost:8001/user/${id}`)
+    const response = await axios.delete(`${baseUrl}/user/${id}`)
     return response
 }
 
 export const banUserServ = async(id)=>{
-    const response = await axios.put(`http://localhost:8001/user/ban/${id}`)
+    const response = await axios.put(`${baseUrl}/user/ban/${id}`)
     console.log('here')
     return response
 }
 
 export const resetPasswordServ = async(user)=>{
-    const response = await axios.post('http://localhost:8001/user/reset-password', user)
+    const response = await axios.post(`${baseUrl}/user/reset-password`, user)
     return response
 }
 
 export const verifyPasswordServ = async(token)=>{
-    const response = await axios.post('http://localhost:8001/user/verify-password', {token})
+    const response = await axios.post(`${baseUrl}/user/verify-password`, {token})
     return response
 }
 
 export const BorrowBookServ = async(user, book)=>{
-    const response = await axios.post(`http://localhost:8001/user/${user}/borrow/${book}`)
+    const response = await axios.post(`${baseUrl}/user/${user}/borrow/${book}`)
     return response
 }
 
 export const ReturnBookServ = async(user, book)=>{
-    const response = await axios.post(`http://localhost:8001/user/${user}/return/${book}`)
+    const response = await axios.post(`${baseUrl}/user/${user}/return/${book}`)
     return response
 }
 
 export const QueueBookServ = async(user, book)=>{
-    const response = await axios.post(`http://localhost:8001/user/${user}/queue/${book}`)
+    const response = await axios.post(`${baseUrl}/user/${user}/queue/${book}`)
     return response
 }
