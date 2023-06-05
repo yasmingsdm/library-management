c405c27aad4f009b9e10a717da4293fc8cabacc8
Yasmin Dal Medico <yasmin.dm@gmail.com>
2023-06-05 09:33:59 +0200

    memory store

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,8 @@ const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const cors = require('cors')
+const session = require('express-session')
+const MemoryStore = require('memorystore')(session)
 
 const userRouter = require('./routes/users')
 const adminRouter = require('./routes/admin')
@@ -12,6 +14,18 @@ const connectDB = require('./config/db')
 
 
 const app = express()
+
+
+
+app.use(session({
+    cookie: { maxAge: 86400000 },
+    store: new MemoryStore({
+      checkPeriod: 86400000 // prune expired entries every 24h
+    }),
+    resave: false,
+    secret: 'keyboard cat'
+}))
+
 app.use(cors({
     origin: 'http://localhost:3000',
     credentials: true,
